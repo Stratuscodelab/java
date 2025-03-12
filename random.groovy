1c8ca50dc921c49e04bcbddb07a535e10bcf891a
stratuscodelab <beatertech@gmail.com>
2025-03-12 09:58:28 +0000

    random script updated to show random emails and random user generation under the jmeter tests.

--- random.groovy
+++ random.groovy
@@ -1,9 +1,19 @@
-//generating random username
-
+// Generating random username and email
 def randomUser = "user_" + (Math.random() * 1000).toInteger()
+def randomEmail = "user_" + (Math.random() * 1000).toInteger() + "@localhost"
 
-//store in jmerter variable 
+// Store in JMeter variables
 vars.put("randomUser", randomUser)
+vars.put("randomEmail", randomEmail)
 
-//print for debug
-log.info("generated user: " + randomUser)
+// Print for debugging
+log.info("Generated user: " + randomUser)
+log.info("Generated email: " + randomEmail)
+
+
+
+
+//This script in groovy will create a random username and random email, then store them in JMeter variables.
+// the output would be an error due to there not being any official domain, because it's @localhost
+// an example of the output would be GET http://localhost/api/login?username=user_137email=user_399@localhost  within request body.
+// the vars.put can only hold one set of variables same goes for the log.info
