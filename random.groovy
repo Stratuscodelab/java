d57220e63d9fefa79b7fd9bd0c1db3825b5ae1a6
stratuscodelab <beatertech@gmail.com>
2025-03-12 09:58:52 +0000

    random script updated to show random emails and random user generation under the jmeter tests.

--- random.groovy
+++ random.groovy
@@ -17,3 +17,4 @@ log.info("Generated email: " + randomEmail)
 // the output would be an error due to there not being any official domain, because it's @localhost
 // an example of the output would be GET http://localhost/api/login?username=user_137email=user_399@localhost  within request body.
 // the vars.put can only hold one set of variables same goes for the log.info
+// this script should be run in the jmeter test plan and will not work if run in the groovy console.
